66626c47105512275395e9bb790123ab0aadce9b
MarinusEIgenraam <rinuseigenraam@gmail.com>
2021-12-19 11:57:22 +0100

    initial set-up

---
+++ src/main/java/com/willpowered/eindprojectwpsbe/EindprojectWpsBeApplication.java
@@ -0,0 +1,13 @@
+package com.willpowered.eindprojectwpsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindprojectWpsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindprojectWpsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willpowered/eindprojectwpsbe/EindprojectWpsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willpowered.eindprojectwpsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindprojectWpsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
